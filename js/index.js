6e99f83cb3f6afb3427a3d3daa13bbfef59d4318
yandrakarine <yandrakarine.dev@outlook.com>
2022-03-30 19:18:37 -0300

    fields validate,method add new product, creating array of products

--- js/index.js
+++ js/index.js
@@ -6,6 +6,16 @@ class Produto {
   }
   salvar() {
     let produto = this.lerDados();
+
+    if (this.validaCampos(produto)) {
+      this.adicionar(produto)
+    }
+    console.log(this.arrayProdutos)
+  }
+
+  adicionar(produto) {
+    this.arrayProdutos.push(produto);
+    this.id++;
   }
 
   lerDados() {
@@ -16,6 +26,24 @@ class Produto {
     produto.valor = document.getElementById('valor').value;
 
     return produto
+  }
+
+  validaCampos(produto) {
+    let errorAlert = '';
+    if (produto.nomeProduto == '') {
+      errorAlert += '- Informe o nome do produto \n';
+    }
+
+    if (produto.valor == '') {
+      errorAlert += '- Informe o valor do produto';
+    }
+
+    if (errorAlert != '') {
+      alert(errorAlert);
+      return false
+    }
+
+    return true;
   }
 
   cancelar() {
