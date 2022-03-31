a1acdbc85f6575d3f83971a0c463ff26e43ab6e6
yandrakarine <yandrakarine.dev@outlook.com>
2022-03-31 15:41:55 -0300

    insert values into table dynamically and implementation of cancel method (button)

--- js/index.js
+++ js/index.js
@@ -4,13 +4,43 @@ class Produto {
     this.id = 1;
     this.arrayProdutos = []
   }
+
   salvar() {
     let produto = this.lerDados();
 
     if (this.validaCampos(produto)) {
       this.adicionar(produto)
     }
-    console.log(this.arrayProdutos)
+    this.listaTabela();
+    this.cancelar();
+  }
+
+  listaTabela() {
+    let tbody = document.getElementById('tbody')
+    tbody.innerText = ''
+
+    for (let i = 0; i < this.arrayProdutos.length; i++) {
+      let tr = tbody.insertRow();
+
+      let td_id = tr.insertCell();
+      let td_produto = tr.insertCell();
+      let td_valor = tr.insertCell();
+      let td_acoes = tr.insertCell();
+
+      td_id.innerText = this.arrayProdutos[i].id;
+      td_produto.innerText = this.arrayProdutos[i].nomeProduto;
+      td_valor.innerText = this.arrayProdutos[i].valor;
+
+      let imgEdit = document.createElement('img');
+      imgEdit.src = 'assets/img/editar.png';
+      td_acoes.appendChild(imgEdit);
+
+      let imgDelete = document.createElement('img');
+      imgDelete.src = 'assets/img/deletar-lixeira.png';
+      td_acoes.appendChild(imgDelete)
+
+    }
+
   }
 
   adicionar(produto) {
@@ -47,6 +77,8 @@ class Produto {
   }
 
   cancelar() {
+    document.getElementById('produto').value = '';
+    document.getElementById('valor').value = '';
   }
 }
 
